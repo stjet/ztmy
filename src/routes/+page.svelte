13cf8c6bee310adcfe5bde6fed8030e52d2dc13d
jetstream0 <49297268+jetstream0@users.noreply.github.com>
2023-04-05 22:34:13 -0700

    boilerplate

---
+++ src/routes/+page.svelte
@@ -0,0 +1,16 @@
+<div id="main-container">
+  <h1 id="zutomayo-eng">ZUTOMAYO</h1>
+  <h1 id="zutomayo-jap">ずっと真夜中でいいのに</h1>
+</div>
+
+<style>
+  @keyframes cycle {
+   }
+
+  #main-container {
+    width: 60%;
+    margin: auto;
+    margin-top: 25vh;
+    text-align: center;
+  }
+</style>
