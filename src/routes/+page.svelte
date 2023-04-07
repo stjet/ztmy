707198922270867c4156bf0fae64938f48a4c3a4
jetstream0 <49297268+jetstream0@users.noreply.github.com>
2023-04-06 22:51:43 -0700

    title animation

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,16 +1,67 @@
 <div id="main-container">
-  <h1 id="zutomayo-eng">ZUTOMAYO</h1>
-  <h1 id="zutomayo-jap">ずっと真夜中でいいのに</h1>
+  <h1 id="zutomayo-eng"></h1>
 </div>
 
 <style>
-  @keyframes cycle {
-   }
-
   #main-container {
     width: 60%;
     margin: auto;
     margin-top: 25vh;
     text-align: center;
+  }
+
+  @keyframes slidedown {
+    0% {
+      transform: translate(0px, 0px);
+      opacity: 1;
+    }
+    20% {
+      transform: translate(0px, 0px);
+      opacity: 1;
+    }
+    80% {
+      transform: translate(0px, 50px);
+      opacity: 0;
+    }
+    100% {
+      transform: translate(0px, 50px);
+      opacity: 0;
+    }
+   }
+
+  #zutomayo-eng {
+    animation-name: slidedown;
+    animation-duration: 3s;
+    animation-direction: alternate;
+    animation-iteration-count: infinite;
+  }
+
+  @keyframes changecontent {
+    0% {
+      content: "ZUTOMAYO";
+    }
+    24% {
+      content: "ZUTOMAYO";
+    }
+    25% {
+      content: "ずっと真夜中でいいのに";
+    }
+    75% {
+      content: "ずっと真夜中でいいのに";
+    }
+    76% {
+      content: "ZUTOMAYO";
+    }
+    100% {
+      content: "ZUTOMAYO";
+    }
+   }
+
+  #zutomayo-eng::after {
+    content: "ZUTOMAYO";
+    animation-name: changecontent;
+    animation-duration: 12s;
+    animation-iteration-count: infinite;
+    animation-timing-function: steps(1, jump-none);
   }
 </style>
